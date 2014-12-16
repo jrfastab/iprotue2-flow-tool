99c4d148d361bb1c38297a0905851809c4f1781a
John Fastabend <john.r.fastabend@intel.com>
2014-12-16 09:45:09 -0800

    flow: add headers

--- test/models/better_pipeline.h
+++ test/models/better_pipeline.h
@@ -1,3 +1,29 @@
+/*******************************************************************************
+
+  Better Pipeline - A fictional pipeline model for testing
+  Copyright(c) 2014 Intel Corporation.
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms and conditions of the GNU General Public License,
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+
+  The full GNU General Public License is included in this distribution in
+  the file called "COPYING".
+
+  Author: John Fastabend <john.r.fastabend@intel.com>
+
+*******************************************************************************/
+
+
 #ifndef _MY_PIPELINE_H_
 #define _MY_PIPELINE_H_
 
